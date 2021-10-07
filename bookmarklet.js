56d95f5d56b88f98c67ff5105cd62f39253ae8ef
moonyoulove <charlie851213@gmail.com>
2021-10-07 17:02:17 +0800

    fix bug
    更改組數會自動隨機分配，所以清除要在調組數之後進行。

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,46 +3,64 @@
         ["第一組組員一", ["第一組組員二","第一組組員別名"]],
         ["第二組組員一"],
         ["第三組組員一"]
-    ] */
+    ]
+    [[],["齡慶"],[]] */
     const groupList = JSON.parse(prompt("輸入組員名單(JSON格式)", "[]"));
-    const clearBtn = document.querySelector("[aria-label='清除']");
-    clearBtn.click();
-    await waitForMutation(!clearBtn.disabled, clearBtn, { attributes: true, attributeFilter: ["disabled"] });
     const numField = document.querySelector("[aria-label='聊天室數量'");
     const needsWait = numField.value < groupList.length;
     numField.value = groupList.length;
     numField.dispatchEvent(new FocusEvent("blur"));
     const groupTab = document.querySelector("[data-sub-panel-id='4']");
     await waitForMutation(needsWait, groupTab, { childList: true, subtree: true });
+    const clearBtn = document.querySelector("[aria-label='清除']");
+    clearBtn.click();
+    await waitForMutation(!clearBtn.disabled, clearBtn, { attributes: true, attributeFilter: ["disabled"] });
+    // await waitForMutation(!clearBtn.disabled, groupTab, { childList: true, subtree: true}, checkClear);
     const groupBlocks = document.querySelectorAll("[aria-label='會議室名稱']");
-    groupList.forEach((memberNames, index) => {
-        const groupBlock = groupBlocks[index];
-        groupBlock.textContent = `第${index + 1}組`;
+    for (let i = 0; i < groupList.length; i++) {
+        const memberNames = groupList[i];
+        const groupBlock = groupBlocks[i];
+        groupBlock.textContent = `第${i + 1}組`;
         groupBlock.dispatchEvent(new FocusEvent("blur"));
-        memberNames.forEach(memberName => {
+        for (let memberName of memberNames) {
             const aliaNames = Array.isArray(memberName) ? memberName : [memberName];
             for (let name of aliaNames) {
-                const memberBlock = document.querySelector(`[draggable='true'][aria-label='${name}']`);
-                if (memberBlock) {
-                    const dataTransfer = new DataTransfer();
-                    memberBlock.dispatchEvent(new DragEvent("dragstart", { dataTransfer, bubbles: true }));
-                    groupBlock.dispatchEvent(new DragEvent("drop", { dataTransfer, bubbles: true }));
-                    break;
+                if(name) {
+                    const memberBlocks = document.querySelectorAll(`[draggable='true'][aria-label*='${name}']`);
+                    for (let memberBlock of memberBlocks) {
+                        const dataTransfer = new DataTransfer();
+                        memberBlock.dispatchEvent(new DragEvent("dragstart", { dataTransfer, bubbles: true }));
+                        groupBlock.dispatchEvent(new DragEvent("drop", { dataTransfer, bubbles: true }));
+                    }
                 }
             }
-        });
-    });
+        }
+    }
 })();
 
-function waitForMutation(needsWait, target, options) {
+function waitForMutation(needsWait, target, options, condition) {
     return new Promise(resolve => {
         if (needsWait) {
             (new MutationObserver((records, observer) => {
-                observer.disconnect();
-                resolve();
+                if(!condition || condition()) {
+                    observer.disconnect();
+                    resolve();
+                }
+                
             })).observe(target, options);
         } else {
             resolve();
         }
     });
+}
+
+function wait(time) {
+    return new Promise(resolve => {
+        setTimeout(resolve, time);
+    });
+}
+
+function checkClear() {
+    const memberNums = document.querySelectorAll("[aria-label$='位參與者']");
+    return Array.prototype.every.call(memberNums, memberNum => memberNum.ariaLabel === "0 位參與者")
 }
